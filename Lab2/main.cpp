ac4ae64ea23108c8909777d5af54c95614d29271
Антон Жидович <toni.zhidovich@mail.ru>
2022-03-01 19:50:23 +0300

    Created generation of array

--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <windows.h>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+void printArr(int* arr, int n){
+    for(int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    system("pause");
+    srand(time(0));
+    int n = 5 + rand() % 15;
+    int* arr = new int[n];
+    for(int i = 0; i < n; i++){
+        arr[i] = rand();
+    }
+    printf("Generated array of %d elements: \n", n);
+    printArr(arr, n);
+
     return 0;
 }
