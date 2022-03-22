b5ce8a3226d688dca6885967706284805c758b2d
Антон Жидович <toni.zhidovich@mail.ru>
2022-03-22 19:29:25 +0300

    Created array and critical section

--- Lab3/main.cpp
+++ Lab3/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
-
+#include <time.h>
+#include <windows.h>
+CRITICAL_SECTION cs;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n;
+    srand(time(0));
+    n = 10 + rand() % 21; //from 10 to 30 elems
+    int* arr = new int[n];
+    for(int i = 0; i < n; i++){
+        arr[i] = 0;
+    }
+    int threadCount = 3 + rand() % 6; // from 3 to 8 threads
+
+    HANDLE startThreadsEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
+    InitializeCriticalSection(&cs);
+    DeleteCriticalSection(&cs);
     return 0;
 }
