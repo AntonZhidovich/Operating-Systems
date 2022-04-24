1c869e958f4c18afcd2d3d6283f8ad83732b8ec6
Антон Жидович <toni.zhidovich@mail.ru>
2022-04-24 19:37:39 +0300

    Added thread stop handling

--- Lab3-C++17/main.cpp
+++ Lab3-C++17/main.cpp
@@ -11,11 +11,51 @@ std::mutex startMutex;
 bool ready;
 std::mutex markerMutex;
 
-void marker(int num){
+bool *threadsPaused;
+std::condition_variable stateChanged;
+std::mutex stateMutex;
+struct threadArgs{
+    int* arr;
+    int n;
+    int num;
+    bool actions[2];
+    threadArgs(int* _arr, int _n, int _num) : arr(_arr), num(_num),  n(_n){
+    }
+};
+
+bool allPaused(bool* arr, int n){
+    for(int i = 0; i < n; i++){
+        if(!arr[i])
+            return false;
+    }
+    return true;
+}
+
+void marker(threadArgs* args){
     std::unique_lock<std::mutex> startlk(startMutex);
     startCV.wait(startlk, []()->bool{return ready;});
-    srand(num);
-    printf("Thread #%d is started.\n", num);
+    startlk.unlock();
+    srand(args->num);
+    printf("Thread #%d is started.\n", args->num);
+    int count = 0;
+
+    while (true) {
+        std::unique_lock<std::mutex> lk(markerMutex);
+        int i = rand() % args->n;
+        if (args->arr[i] == 0) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(5));
+            args->arr[i] = args->num;
+            ++count;
+            lk.unlock();
+            std::this_thread::sleep_for(std::chrono::milliseconds(5));
+        } else{
+            printf("Thread #%d. Marked %d elems. Unable to mark a[%d].\n", args->num, count, i);
+            lk.unlock();
+            threadsPaused[args->num - 1] = true;
+            stateChanged.notify_all();
+            break;
+        }
+    }
 }
 
 void printArray(int* arr, int n){
@@ -40,12 +80,22 @@ int main(){
     int threadCount = 3 + rand() % 6; // from 3 to 8 threads
     std::cout << threadCount << std::endl;
     std::vector<std::thread> threads(threadCount);
+    std::vector<threadArgs*> argsVec(threadCount);
+    threadsPaused = new bool[threadCount];
     for(int i = 0; i < threadCount; i++){
-        threads.push_back(std::thread(marker, i+1));
+        argsVec.push_back(new threadArgs(arr, n, i+1));
+        threads.push_back(std::thread(marker, argsVec.back()));
         threads.back().detach();
     }
     ready = true;
     startCV.notify_all();
-    std::this_thread::sleep_for(std::chrono::seconds(2));
+
+    int terminatedCount = 0;
+    while(terminatedCount != threadCount){
+        std::unique_lock<std::mutex> lk(stateMutex);
+        stateChanged.wait(lk, [=](){return allPaused(threadsPaused, threadCount);});
+        //printArray(arr, n);
+        break;
+    }
     return 0;
 }
